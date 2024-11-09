fe3b0b003f9442da02d78acbb2134474db97fa4b
cybaek <vovoboss@gmail.com>
2024-11-10 08:25:38 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,39 @@
 #![allow(unused_imports)]
 
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // Uncomment this block to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buffer = [0; 512];
-                loop {
-                    let read_count = stream.read(&mut buffer).unwrap();
-                    if read_count == 0 {
-                        break;
+    loop {
+        match listener.accept().await {
+            Ok((mut socket, _)) => {
+                tokio::spawn(async move {
+                    let mut buffer = [0; 512];
+                    loop {
+                        match socket.read(&mut buffer).await {
+                            Ok(0) => return,
+                            Ok(_) => {
+                                if let Err(e) = socket.write_all(b"+PONG\r\n").await {
+                                    eprintln!("{:?}", e);
+                                    return;
+                                }
+                            }
+                            Err(e) => {
+                                eprintln!("error reading from socket; err = {:?}", e);
+                                return;
+                            }
+                        }
                     }
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
+                });
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            Err(e) => println!("couldn't get client: {:?}", e),
         }
     }
 }
