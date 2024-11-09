6bcf99e5ef49208da239bdc19f316902f5d5fd28
cybaek <vovoboss@gmail.com>
2024-11-10 06:53:07 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
 #![allow(unused_imports)]
+
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     // Uncomment this block to pass the first stage
     //
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                let mut buffer = [0; 512];
+                loop {
+                    let read_count = stream.read(&mut buffer).unwrap();
+                    if read_count == 0 {
+                        break;
+                    }
+                    stream.write(b"+PONG\r\n").unwrap();
+                }
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
